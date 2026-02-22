a1df515c13ce200630409cd394f613de3080514d
Meghanagowd <gowdameghana256@gmail.com>
2026-02-22 23:34:57 +0530

    Day 6: Implemented Palindrome number check (GFG)
    - Reversed number using while loop
    - Compared original and reversed values

---
+++ Palindrome.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static boolean isPalindrome(int n) {
+
+        int original = n;
+        int reverse = 0;
+
+        while(n != 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        if(isPalindrome(n))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
